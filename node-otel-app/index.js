a2cf85e0a1a0c37cf98fccafe3e1ea375e003cf6
Enrique García <enrique.garcia.saez.tech@bbva.com>
2026-04-18 10:16:02 +0200

    improve span name

--- node-otel-app/index.js
+++ node-otel-app/index.js
@@ -9,30 +9,33 @@ const {
 } = require('@opentelemetry/api');
 
 const app = express();
-const tracer = trace.getTracer('api-gateway');
+const tracer = trace.getTracer(process.env.OTEL_SERVICE_NAME);
 
 app.get('/grouped-posts-by-category', async (req, res) => {
-  await tracer.startActiveSpan('node-otel-app', async (span) => {
-    let headers = {};
-    // Inject current trace context into headers
-    propagation.inject(context.active(), headers);
+  await tracer.startActiveSpan(
+    'handling grouped-posts-by-category request',
+    async (span) => {
+      let headers = {};
+      // Inject current trace context into headers
+      propagation.inject(context.active(), headers);
 
-    try {
-      const groupedPosts = await fetch(
-        `http://localhost:1337/api/grouped-posts-by-category`,
-        { headers },
-      ).then((res) => res.json());
+      try {
+        const groupedPosts = await fetch(
+          `http://localhost:1337/api/grouped-posts-by-category`,
+          { headers },
+        ).then((res) => res.json());
 
-      res.json(groupedPosts);
-    } catch (error) {
-      span.recordException(error);
-      span.setStatus({ code: SpanStatusCode.ERROR });
+        res.json(groupedPosts);
+      } catch (error) {
+        span.recordException(error);
+        span.setStatus({ code: SpanStatusCode.ERROR });
 
-      res.status(500).json({ error: error.message });
-    }
+        res.status(500).json({ error: error.message });
+      }
 
-    span.end();
-  });
+      span.end();
+    },
+  );
 });
 
 app.listen(3000, () => {
